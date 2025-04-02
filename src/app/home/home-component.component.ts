a688cff70d01753f97288ca640c1b2ccf1bb2648
Keni Royer Santiago <keni.santiago@iecm.mx>
2025-04-01 19:06:12 -0600

    Se introduce el boton de descarga de codigo de QR

--- src/app/home/home-component.component.ts
+++ src/app/home/home-component.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { QRCodeComponent } from "angularx-qrcode";
 import { FormsModule } from "@angular/forms";
+import { SafeUrl } from "@angular/platform-browser";
 
 @Component({
   templateUrl: './home-component.component.html',
@@ -15,11 +16,20 @@ export class HomeComponent {
   public myQrCode : string = '';
   public inputUrl : string = '';
 
+  public myAngularxQrCode: string = "";
+  public qrCodeDownloadLink: SafeUrl = "";
+
+  constructor () {
+    this.myAngularxQrCode = 'Your QR code data string';
+  }
+
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+  }
+
   getQr():string {
     console.log(this.inputUrl);
     return this.inputUrl;
   }
-
-
 
 }
